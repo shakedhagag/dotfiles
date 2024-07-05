4d11a1c0c75189ed7a258681eae5abd21330b84e
Shaked Hagag <shaked.hagag@openweb.com>
2024-07-05 18:23:05 +0300

    enable copilot

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -2,11 +2,14 @@ return {
   -- This is the copilot-cmp plugin
   {
     "zbirenbaum/copilot-cmp",
-    enabled = false,
+    enabled = true,
     event = { "BufEnter" },
     dependencies = { "zbirenbaum/copilot.lua" },
     config = function()
       require("copilot_cmp").setup()
     end,
+  },
+  {
+    "github/copilot.vim",
   },
 }
