e4c81f73cc7b10e5fd8280ec988b23216a22fd17
Shaked Hagag <shaked.hagag@openweb.com>
2024-12-12 13:46:01 +0200

    updates, and coloscheme change

--- nvim/.config/nvim/lua/plugins/astroui.lua
+++ nvim/.config/nvim/lua/plugins/astroui.lua
@@ -9,7 +9,8 @@ return {
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
-    colorscheme = "carbonfox",
+    colorscheme = "kanagawa",
+    transparent = true,
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,5 @@
 return {
   { "rose-pine/neovim", name = "rose-pine" },
   { "EdenEast/nightfox.nvim", name = "nightfox" },
+  { "rebelot/kanagawa.nvim", name = "kanagawa" },
 }
