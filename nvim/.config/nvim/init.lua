5f0b7c65512dbbd7c608bf2006f47f750d3cfe01
Shaked Hagag <shaked.hagag@openweb.com>
2024-12-31 17:43:47 +0200

    splash screen

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,9 +6,17 @@ if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
--- vim.api.nvim_create_autocmd("VimEnter", {
---   callback = function() vim.cmd "hi NeoTreeNormal guibg=NONE cterm=NONE" end,
--- })
+
+-- Auto command to set the background of Telescope
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function() vim.cmd "hi TelescopeNormal guibg=NONE cterm=NONE" end,
+})
+
+-- Auto command to set the border of Telescope
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function() vim.cmd "hi TelescopeBorder guibg=NONE cterm=NONE" end,
+})
+
 -- validate that lazy is available
 if not pcall(require, "lazy") then
   -- stylua: ignore
