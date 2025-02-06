6ab9176fcfa8151ce2445b7e6d472a8ca1f0cc80
Shaked Hagag <shaked.hagag@openweb.com>
2025-02-06 11:25:24 +0200

    inlay-hints, move line mappings, ghostty alt as opt

--- nvim/.config/nvim/lua/plugins/gomove.lua
+++ nvim/.config/nvim/lua/plugins/gomove.lua
@@ -1,13 +1,30 @@
 return {
   {
-    "willothy/moveline.nvim",
-    build = "make",
-    config = function()
-      local moveline = require "moveline"
-      vim.keymap.set("n", "<M-k>", moveline.up)
-      vim.keymap.set("n", "<M-j>", moveline.down)
-      vim.keymap.set("v", "<M-k>", moveline.block_up)
-      vim.keymap.set("v", "<M-j>", moveline.block_down)
-    end,
+    "fedepujol/move.nvim",
+    opts = {
+      --- Config
+    },
+    keys = {
+      { "<A-j>", ":MoveLine(1)<CR>", mode = "n", desc = "Move line down" },
+      { "<A-k>", ":MoveLine(-1)<CR>", mode = "n", desc = "Move line up" },
+      { "<leader>wf", ":MoveWord(1)<CR>", mode = "n", desc = "Move word forward" },
+      { "<leader>wb", ":MoveWord(-1)<CR>", mode = "n", desc = "Move word backward" },
+
+      { "<A-j>", ":MoveBlock(1)<CR>", mode = "v", desc = "Move block down" },
+      { "<A-k>", ":MoveBlock(-1)<CR>", mode = "v", desc = "Move block up" },
+      { "<A-h>", ":MoveHBlock(-1)<CR>", mode = "v", desc = "Move block left" },
+      { "<A-l>", ":MoveHBlock(1)<CR>", mode = "v", desc = "Move block right" },
+    },
   },
+  -- {
+  --   "willothy/moveline.nvim",
+  --   build = "make",
+  --   config = function()
+  --     local moveline = require "moveline"
+  --     vim.keymap.set("n", "<C-k>", moveline.up)
+  --     vim.keymap.set("n", "<M-j>", moveline.down)
+  --     vim.keymap.set("v", "<M-k>", moveline.block_up)
+  --     vim.keymap.set("v", "<M-j>", moveline.block_down)
+  --   end,
+  -- },
 }
